e23008e56af16abc64df7505f207ec767eee7ae4
orlandotorres <orlando.torres12@upr.edu>
2020-08-20 10:04:58 -0400

    Changed the chunk size to 2 songs per request to avoid Heroku time out.

--- static/js/script.js
+++ static/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 
                 if (data != null) {
 
-                    classifyMusic(data, 5, returnResultsToSpotifyWizard);
+                    classifyMusic(data, 2, returnResultsToSpotifyWizard);
 
                 }
                 else {
